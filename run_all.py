f20fbebbc00f81566f96ffcbd5670d4a04e2fa4a
BLienkamp <80453213+BLienkamp@users.noreply.github.com>
2024-10-31 17:55:50 +0100

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -8,22 +8,13 @@ import os
 
 if __name__ == "__main__":
     route = sys.argv[1]  # 55 or 155
-    route_time_text = sys.argv[2] # for 55: '18_56_00' or '13_50_00'; for 155: '13_50_00' or '18_56_00'
+    route_time_text = sys.argv[2] # for 55: '18_56_00' or '15_58_00'; for 155: '13_50_00' or '07_44_30'
     comp_perc = sys.argv[3] # 0 - 1
     num_scenarios = int(sys.argv[4]) + 1 # 0 - 40
     walking_distance = int(sys.argv[5]) # [100, 150, 250, 300, 350, 400]
     util_value = int(sys.argv[6]) # 750
     algorithm = sys.argv[7] # exact, heuristic, or greedy
     seed = int(sys.argv[8]) # integer
-
-    # route = 55  # 55 or 155
-    # route_time_text = '18_56_00' # for 55: '18_56_00' or '13_50_00'; for 155: '13_50_00' or '18_56_00'
-    # comp_perc = 1 # 0 - 1
-    # num_scenarios = 2 # 0 - 40
-    # walking_distance = 250 # [100, 150, 250, 300, 350, 400]
-    # util_value = 750 # 750
-    # algorithm = 'greedy' # exact, heuristic, or greedy
-    # seed = 0 # integer
 
     # Pull num_scenarios random scenarios out of our sample set of 50 scenarios with a fixed seed
     random.seed(seed)
